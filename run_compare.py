4a1826c6a01a1e9cfbe6aad16da3ec5d70e0b83a
Mateusz Konieczny <matkoniecz@gmail.com>
2020-09-02 18:19:38 +0200

    add on disk caching

--- run_compare.py
+++ run_compare.py
@@ -5,18 +5,35 @@ from geopy import distance
 import geopy
 import configparser
 import time
+from diskcache import Cache 
 
 from dataset import testing_dataset
 
-def get_geocoder_reply(geocoder, query):
-    while True:
-        time.sleep(1)
-        try:
-            return geocoder.geocode(query)
-        except geopy.exc.GeocoderUnavailable:
-            print("retrying")
-            time.sleep(10)
-            continue
+class CachedGeocoder():
+    def __init__(self, geocoder_from_geopy, identifier):
+        self.geocoder_from_geopy = geocoder_from_geopy
+        self.identifier = identifier
+        self.cache = Cache('tmp/' + identifier)
+
+    def get_geocoder_reply(self, query):
+        returned = self.cache.get(query)
+        if returned != None:
+            return returned['reply']
+        else:
+            uncached = self.get_uncached_geocoder_reply(query)
+            self.cache.set(query, {'reply': uncached})
+            return uncached
+
+    def get_uncached_geocoder_reply(self, query):
+        print(query, "for", self.identifier, "was not cached")
+        while True:
+            time.sleep(1)
+            try:
+                return self.geocoder_from_geopy.geocode(query)
+            except geopy.exc.GeocoderUnavailable:
+                print("retrying")
+                time.sleep(10)
+                continue
 
 def osm_url_pin(lat, lon):
     return "https://www.openstreetmap.org/?mlat=" + str(lat) + "&mlon=" + str(lon) + "#map=18/" + str(lat) + "/" + str(lon)
@@ -29,16 +46,22 @@ def main():
         print("insufficient data in config file!")
         print("please run `python3 create_config_file.py`")
         return
+    email = config['DEFAULT']['email']
+    apis = create_geocoders(email)
+    run_compare(apis)
 
-    user_agent = config['DEFAULT']['email'] + " using geocode benchmark https://github.com/matkoniecz/benchmark-geocoders"
-    apis = [
-        Nominatim(user_agent=user_agent),
-        Photon(user_agent=user_agent),
+def create_geocoders(email):
+    user_agent = email + " using geocode benchmark https://github.com/matkoniecz/benchmark-geocoders"
+    return [
+        CachedGeocoder(Nominatim(user_agent=user_agent), "nominatim"),
+        CachedGeocoder(Photon(user_agent=user_agent), "photon"),
     ]
 
+def run_compare(apis):
     for entry in testing_dataset():
         for geocoder in apis:
-            location = get_geocoder_reply(geocoder, entry['query'])
+            print(entry, geocoder.identifier)
+            location = geocoder.get_geocoder_reply(entry['query'])
             if location == None:
                 if entry['lat'] == None and entry['lon'] == None:
                     print("nothing was found as expected")
